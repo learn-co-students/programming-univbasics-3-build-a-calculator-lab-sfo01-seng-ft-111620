865e9993132dee30bcc381544c47b592a141c3c3
Jeremy Rodriguez <jeremy.rodrguez@yahoo.com>
2020-10-12 03:54:34 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,35 @@
 def addition(num1, num2)
-
+  puts num2 + num1
+  result = num2 + num1
+  return result
 end
 
 def subtraction(num1, num2)
-
+  puts num1 - num2
+  results = num1 - num2
+  return results
 end
 
 def division(num1, num2)
-
+  puts num1 / num2
+  resultz = num1 / num2
+  return resultz
 end
 
 def multiplication(num1, num2)
-
+  puts num1 * num2
+  result_m = num1 * num2
+  return result_m
 end
 
 def modulo(num1, num2)
-
+  puts num1 % num2
+  result = num1 % num2
+  return result
 end
 
 def square_root(num)
-
+  puts Math.sqrt(num)
+  result = Math.sqrt(num)
+  return Math.sqrt(num)
 end
