c5faf8291bad9edacc31725cda1bddf273a9a26c
searching-block-4535 <juancmezat@gmail.com>
2020-10-27 04:57:54 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,24 @@
 def addition(num1, num2)
+  sum = num1 + num2
 
 end
 
 def subtraction(num1, num2)
-
+  diff = num1 - num2
 end
 
 def division(num1, num2)
-
+  div = num1/num2
 end
 
 def multiplication(num1, num2)
-
+  prod = num1 * num2
 end
 
 def modulo(num1, num2)
-
+  mod = num1%num2
 end
 
 def square_root(num)
-
+  sqr = num**0.5
 end
